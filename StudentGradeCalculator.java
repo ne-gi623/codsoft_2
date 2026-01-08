5d870ea01b7f732e8ea2c4e712e4e8213092402e
Jagdish Singh Bisht <jagdishbisht10192@gmail.com>
2026-01-08 21:57:21 +0530

    CODSOFT Task 2 - Student Grade Calculator

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = scanner.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter marks for subject " + i + " (out of 100): ");
+            int marks = scanner.nextInt();
+
+            
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks! Please enter marks between 0 and 100.");
+                i--; // retry same subject
+                continue;
+            }
+
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / subjects;
+
+        String grade;
+
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("\n RESULT ");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
